06e3fb4d7999c055bf81206dfb012c1ecc7b8bcf
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-07-18 12:42:40 +0530

    Removed boilerplate code using lombok.

--- restful-wed-services/src/main/java/com/in28minutes/springboot/restfulwedservices/versioning/PersonV1.java
+++ restful-wed-services/src/main/java/com/in28minutes/springboot/restfulwedservices/versioning/PersonV1.java
@@ -1,24 +1,33 @@
 package com.in28minutes.springboot.restfulwedservices.versioning;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class PersonV1 {
 
 	private String name;
 
-	public PersonV1(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "PersonV1 [name=" + name + "]";
-	}
+//	public PersonV1(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "PersonV1 [name=" + name + "]";
+//	}
 	
 }
--- restful-wed-services/src/main/java/com/in28minutes/springboot/restfulwedservices/versioning/PersonV2.java
+++ restful-wed-services/src/main/java/com/in28minutes/springboot/restfulwedservices/versioning/PersonV2.java
@@ -1,24 +1,33 @@
 package com.in28minutes.springboot.restfulwedservices.versioning;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class PersonV2 {
 	private Name name;
 
-	public PersonV2(Name name) {
-		super();
-		this.name = name;
-	}
-
-	public Name getName() {
-		return name;
-	}
-
-	public void setName(Name name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "PersonV2 [name=" + name + "]";
-	}
+//	public PersonV2(Name name) {
+//		super();
+//		this.name = name;
+//	}
+//
+//	public Name getName() {
+//		return name;
+//	}
+//
+//	public void setName(Name name) {
+//		this.name = name;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "PersonV2 [name=" + name + "]";
+//	}
 
 }
